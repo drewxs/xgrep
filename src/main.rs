19a2971179bdf33826eb03c44a7de0f31a72ab51
Andrew X. Shah <drewshah0@gmail.com>
2023-03-26 09:35:36 -0600

    feat: create arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+
+use xgrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Failed to parsed arguments: {err}");
+        process::exit(1);
+    });
+    println!("query: {}", config.query);
+    println!("path: {}", config.file_path);
+
+    if let Err(e) = xgrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
