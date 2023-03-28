82e53bff28a91b82251f99bba16d6fbda5bb3768
Andrew X. Shah <drewshah0@gmail.com>
2023-03-27 21:32:16 -0600

    feat: create case-insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,17 +15,28 @@ impl Config {
             return Err("file path is required");
         }
 
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -43,18 +55,45 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-        let query = "duct";
+    fn case_sensitve() {
+        let query = "Lorem";
         let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.";
+Lorem ipsum dolor sit amet
+consectetur adipiscing elit
+Cras vel augue vel magna
+Donec ultrices et nisilorem";
+        assert_eq!(vec!["Lorem ipsum dolor sit amet"], search(query, contents));
+    }
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    #[test]
+    fn case_insensitive() {
+        let query = "Lorem";
+        let contents = "\
+Lorem ipsum dolor sit amet
+consectetur adipiscing elit
+Cras vel augue vel magna
+Donec ultrices et nisilorem";
+        assert_eq!(
+            vec!["Lorem ipsum dolor sit amet", "Donec ultrices et nisilorem"],
+            search_case_insensitive(query, contents)
+        );
     }
 }
