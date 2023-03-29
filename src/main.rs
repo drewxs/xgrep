772ac2ea7c7fd01e14ebc49685c24b11afb157d0
Andrew X. Shah <drewshah0@gmail.com>
2023-03-28 21:30:36 -0600

    fix: error logging

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Failed to parsed arguments: {err}");
+        eprintln!("Failed to parsed arguments: {err}");
         process::exit(1);
     });
     println!("query: {}", config.query);
     println!("path: {}", config.file_path);
 
     if let Err(e) = xgrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
